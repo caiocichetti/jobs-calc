e572d48b2270f6ffbd63278f5bc5655c7ae3cdee
Caio Cichetti <caio_cichetti@outlook.com>
2021-03-26 21:32:05 -0300

    feat: rendering data in html with ejs

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,20 @@ const routes = express();
 
 const viewsPath = `${__dirname}/views/`;
 
+const profile = {
+  name: 'Caio Cichetti',
+  avatar: 'https://avatars.githubusercontent.com/u/58996814?v=4',
+  'monthly-budget': 2000,
+  'days-per-week': 5,
+  'hours-per-day': 6,
+  'vacation-per-year': 1,
+};
+
 routes.get('/', (req, res) => res.render(`${viewsPath}/index`));
 routes.get('/job', (req, res) => res.render(`${viewsPath}/job`));
 routes.get('/job/edit', (req, res) => res.render(`${viewsPath}/job-edit`));
-routes.get('/profile', (req, res) => res.render(`${viewsPath}/profile`));
+routes.get('/profile', (req, res) =>
+  res.render(`${viewsPath}/profile`, { profile })
+);
 
 module.exports = routes;
